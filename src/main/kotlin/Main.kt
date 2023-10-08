1a7b73381fed9d7c34cd46e80194ed31ac1c7021
user <persimon@inbox.ru>
2023-10-09 01:49:55 +0500

    added 3d model

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,62 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+//import androidx.compose.ui.draw.EmptyBuildDrawCacheParams.size
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Column(
+            modifier = Modifier.fillMaxSize(), //заполняем всё доступное пространство
+            horizontalAlignment = Alignment.CenterHorizontally, //по центру горизонтально
+//            verticalArrangement = Arrangement.Center //и вертикально
+        ) {
+            var offsetX by remember { mutableStateOf(0f) }
+            var offsetY by remember { mutableStateOf(0f) }
+
+            Canvas(modifier = Modifier.fillMaxSize()
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                    }
+                }
+            ){
+                val canvasQuadrantSize = size / 2F
+                drawRect(
+                    topLeft = Offset(offsetX,offsetY),
+                    color = Color.Green,
+                    size = canvasQuadrantSize
+                )
+            }
+        }
+    }
+}
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        App()
+    }
+}
