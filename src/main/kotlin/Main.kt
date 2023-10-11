dffb6df5561662da8538e9b98eedd722ebe3da79
max-sliva <persimon@inbox.ru>
2023-10-11 17:05:31 +0500

    added robot to window

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,15 +13,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 //import androidx.compose.ui.draw.EmptyBuildDrawCacheParams.size
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
-fun App() {
+fun App(quadroPodBody: ImageBitmap) {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
@@ -45,18 +47,36 @@ fun App() {
                 }
             ){
                 val canvasQuadrantSize = size / 2F
-                drawRect(
-                    topLeft = Offset(offsetX,offsetY),
-                    color = Color.Green,
-                    size = canvasQuadrantSize
-                )
+                try {
+//                    val mBitmapFromSdcard = BitmapFactory.decodeFile("/mnt/sdcard/face.png").asImageBitmap()
+//                    val quadroPodBody = useResource("quadroPodBody2.PNG") { loadImageBitmap(it) }
+                    drawImage(
+                        image = quadroPodBody,
+                        topLeft = Offset(0F, 0F)
+                    )
+                    val arm1 = useResource("arm1.PNG") { loadImageBitmap(it) }
+                    drawImage(
+                        image = arm1,
+                        topLeft = Offset(offsetX,offsetY)
+                    )
+//                    println("image size = ${quadroPodBody.width} x ${quadroPodBody.height}")
+                } catch (e: NullPointerException) {
+//                    Toast.makeText(applicationContext,"No image", Toast.LENGTH_LONG).show()
+                    println("No image")
+                }
             }
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    val quadroPodBody = useResource("quadroPodBody2.PNG") { loadImageBitmap(it) }
+    val bodyWidth = quadroPodBody.width
+    val bodyHeight = quadroPodBody.height
+
+    println("image size on start= ${quadroPodBody.width} x ${quadroPodBody.height}")
+    Window(onCloseRequest = ::exitApplication, state = WindowState(size = DpSize(bodyWidth.dp, bodyHeight.dp)) ) {
+//    Window(onCloseRequest = ::exitApplication ) {
+        App(quadroPodBody)
     }
 }
